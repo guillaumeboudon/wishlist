28533e079293a99d188bcc53fe7eb60801ce6204
Guillaume BOUDON <guillaumeboudon@gmail.com>
2021-05-06 22:42:44 +0200

    :sparkles: Implement basic views templates

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
 require "cuba"
+require "cuba/contrib"
+require "mote"
+
+Cuba.plugin Cuba::Mote
 
 Cuba.define do
   on root do
-    res.write "Hello"
+    render("home", title: "Home")
+  end
+
+  on default do
+    res.status = 404
+    render("404", title: "Page not found")
+    halt(res.finish)
   end
 end
